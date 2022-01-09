ff6162ea79758cf56ad62e2402be054d3f016f67
dashaw92 <dashaw92@gmail.com>
2022-01-09 17:45:28 -0500

    Add specialization for records

--- src/main/java/me/danny/qlib/PDCBridge.java
+++ src/main/java/me/danny/qlib/PDCBridge.java
@@ -2,16 +2,16 @@ package me.danny.qlib;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.internal.GsonBuildConfig;
 import org.bukkit.NamespacedKey;
 import org.bukkit.persistence.PersistentDataAdapterContext;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.Plugin;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.util.*;
+import java.io.*;
+import java.util.NoSuchElementException;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 public final class PDCBridge {
     private static final Gson gson = new GsonBuilder()
@@ -19,8 +19,15 @@ public final class PDCBridge {
             .serializeNulls()
             .create();
 
+
+
     public static <T> PersistentDataType<PersistentDataContainer, T> defineType(Plugin plugin, Class<T> clazz) {
-        if(clazz.isRecord()) throw new UnsupportedOperationException("Cannot define a record type. Support may be added with deserializers.");
+        if(clazz.isRecord()) return defineTypeRecord(plugin, clazz);
+        return defineTypeClass(plugin, clazz);
+    }
+
+    private static <T> PersistentDataType<PersistentDataContainer, T> defineTypeClass(Plugin plugin, Class<T> clazz) {
+        if(clazz.isRecord()) throw new UnsupportedOperationException("Cannot define a record type.");
         var key = new NamespacedKey(plugin, "json");
 
         return new PersistentDataType<>() {
@@ -47,6 +54,54 @@ public final class PDCBridge {
             public T fromPrimitive(PersistentDataContainer primitive, PersistentDataAdapterContext context) {
                 String json = primitive.get(key, PersistentDataType.STRING);
                 return gson.fromJson(json, getComplexType());
+            }
+        };
+    }
+
+    private static <T> PersistentDataType<PersistentDataContainer, T> defineTypeRecord(Plugin plugin, Class<T> clazz) {
+        if(!Serializable.class.isAssignableFrom(clazz)) throw new UnsupportedOperationException("Cannot define a record type that does not implement %s.".formatted(Serializable.class.getCanonicalName()));
+        var key = new NamespacedKey(plugin, "bytes");
+
+        return new PersistentDataType<>() {
+
+            @Override
+            public Class<PersistentDataContainer> getPrimitiveType() {
+                return PersistentDataContainer.class;
+            }
+
+            @Override
+            public Class<T> getComplexType() {
+                return clazz;
+            }
+
+            @Override
+            public PersistentDataContainer toPrimitive(T complex, PersistentDataAdapterContext context) {
+                PersistentDataContainer data = context.newPersistentDataContainer();
+                try(var bytes = new ByteArrayOutputStream(); var gzip = new GZIPOutputStream(bytes); var os = new ObjectOutputStream(gzip)) {
+                    os.writeObject(complex);
+                    os.flush();
+                    gzip.close();
+                    var written = bytes.toByteArray();
+                    data.set(key, PersistentDataType.BYTE_ARRAY, written);
+                    return data;
+                } catch(IOException ex) {
+                    plugin.getLogger().warning("[defineTypeRecord::toPrimitive] Failed to serialize record to data container. Nothing has been set.");
+                    return context.newPersistentDataContainer();
+                }
+            }
+
+            @SuppressWarnings("unchecked")
+            @Override
+            public T fromPrimitive(PersistentDataContainer primitive, PersistentDataAdapterContext context) {
+                 var read = primitive.get(key, PersistentDataType.BYTE_ARRAY);
+                 if(read == null) throw new NoSuchElementException("[defineTypeRecord::fromPrimitive] Provided container does not contain a serialized " + getComplexType().getCanonicalName() + "!");
+                 try(var bytes = new ByteArrayInputStream(read); var gzip = new GZIPInputStream(bytes); var is = new ObjectInputStream(gzip)) {
+                    return (T) is.readObject();
+                 } catch(IOException | ClassNotFoundException ex) {
+                     ex.printStackTrace();
+                     plugin.getLogger().warning("[defineTypeRecord::fromPrimitive] Failed to deserialize record from data container. Must return null.");
+                     return null;
+                 }
             }
         };
     }
--- src/main/java/me/danny/qlib/testing/TestPlugin.java
+++ src/main/java/me/danny/qlib/testing/TestPlugin.java
@@ -11,12 +11,15 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.Serializable;
 import java.util.Arrays;
 import java.util.UUID;
 
+@SuppressWarnings("all") //this is just a test plugin to ensure PDCBridge is working
 public final class TestPlugin extends JavaPlugin {
 
     private final PersistentDataType<?, SimpleRecord> SIMPLE_RECORD = PDCBridge.defineType(this, SimpleRecord.class);
+    private final PersistentDataType<?, TestRecord> RECORD = PDCBridge.defineType(this, TestRecord.class);
 
     @Override
     public void onEnable() {
@@ -35,12 +38,15 @@ public final class TestPlugin extends JavaPlugin {
         public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
             if(!(sender instanceof Player p)) return true;
             var key2 = new NamespacedKey(inst, "simple_record");
+            var key = new NamespacedKey(inst, "test_record");
 
             if(p.getInventory().getItemInMainHand().getType() == Material.STICK) {
                 var it = p.getInventory().getItemInMainHand();
                 var meta = it.getItemMeta();
                 var simpleRecordOut = meta.getPersistentDataContainer().get(key2, inst.SIMPLE_RECORD);
+                var recordOut = meta.getPersistentDataContainer().get(key, inst.RECORD);
                 p.sendMessage("Got : " + simpleRecordOut);
+                p.sendMessage("Got : " + recordOut);
                 return true;
             }
 
@@ -54,15 +60,16 @@ public final class TestPlugin extends JavaPlugin {
                     new byte[] { 1, 2, 3 },
                     p.getUniqueId()
             );
+            var record = new TestRecord("hello world!", p.getUniqueId());
 
             meta.getPersistentDataContainer().set(key2, inst.SIMPLE_RECORD, simpleRecord);
+            meta.getPersistentDataContainer().set(key, inst.RECORD, record);
 
             it.setItemMeta(meta);
             p.getInventory().addItem(it);
             return true;
         }
     }
-
     public static final class SimpleRecord {
         private final String stringField;
         private final int someInt;
@@ -86,4 +93,5 @@ public final class TestPlugin extends JavaPlugin {
                     '}';
         }
     }
+    public record TestRecord(String stringField, UUID id) implements Serializable {}
 }
